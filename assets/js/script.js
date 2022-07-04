197d4fe20399749960b367bffbb21c4bde16bc8f
Christopher Antony <christophercarreira2306@gmail.com>
2022-07-04 01:29:12 -0300

    Inserindo Js no portfólio

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+document.getElementById("instagram").addEventListener("click", instagram);
+document.getElementById("gitHub").addEventListener("click", gitHub);
+document.getElementById("linkedIn").addEventListener("click", linkedIn);
+
+function instagram(){
+    window.location.href = "https://www.instagram.com/christopher_antonyc/";
+}
+
+function gitHub(){
+    window.location.href = "https://github.com/christopher2306";
+}
+
+function linkedIn(){
+    window.location.href = "https://www.linkedin.com/in/christopher-antony-355351160/";
+}
+
